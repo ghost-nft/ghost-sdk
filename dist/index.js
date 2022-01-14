fd6ac4cb1527364737d51b5462acf72df7ef9976
ghostpow <ghostnftpow@gmail.com>
2022-01-15 01:34:13 +0900

    fix update from command

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./sdk-test.cjs.production.min.js')
+  module.exports = require('./ghost-sdk.cjs.production.min.js')
 } else {
-  module.exports = require('./sdk-test.cjs.development.js')
+  module.exports = require('./ghost-sdk.cjs.development.js')
 }
